2bd5c30fb5a88794dc3f207073ddeb21d78d2b09
Maggie Koesno <maggie.koesno@gmail.com>
2020-08-21 23:37:16 +0800

    Home Screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Singapore Airlines Lounges"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Image, View } from "react-native";
+import { Avatar, Button, Icon, Layout, Spinner } from "@ui-kitten/components";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Image source={require("../assets/images/sq_banner.png")} />
+      <Button style={styles.button}>Enter Lounge</Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 2,
+  },
+});
