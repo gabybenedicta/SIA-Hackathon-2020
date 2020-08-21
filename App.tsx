104d061c73ea671a2e8595cadc38bf0b5ae19723
gabybenedicta <gaby.benedicta@yahoo.com>
2020-08-21 23:34:55 +0800

    gaby's code

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-
+import * as eva from '@eva-design/eva';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import LoungesScreen from './screens/LoungesScreen';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="Lounges" component={LoungesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
