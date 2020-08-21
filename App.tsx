e7f1b6eaafed0508c0a457c2cddbbb23b701eb03
stephaudrey <stephanieaudreys@gmail.com>
2020-08-22 01:15:36 +0800

    added login

--- App.tsx
+++ App.tsx
@@ -1,23 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest, useAutoDiscovery } from 'expo-auth-session';
+import { Button, Platform } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+WebBrowser.maybeCompleteAuthSession();
+
+const useProxy = Platform.select({ web: false, default: true });
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  // Endpoint
+  const discovery = useAutoDiscovery('https://dev-976155.okta.com/oauth2/default');
+  // Request
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '0oara8r34BlKWfClc4x6',
+      scopes: ['openid', 'profile'],
+      // For usage in managed apps using the proxy
+      redirectUri: makeRedirectUri({
+        // For usage in bare and standalone
+        native: 'https://auth.expo.io/@anonymous/SIAFE2020-62285ed9-6220-4cd4-8d47-e2bff0e80297',
+        useProxy,
+      }),
+    },
+    discovery
+  );
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync({ useProxy });
+      }}
+    />
+  );
 }
